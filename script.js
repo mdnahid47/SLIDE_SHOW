77c50dc3643ad97058e343cb4fd69c74bbcffee4
nahidpathan67 <nmd97243@gmail.com>
2024-02-24 23:13:52 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+
+const slideshowElements = document.querySelectorAll(".slideshow-element");
+
+let countElements = 1;
+
+setInterval(()=>{
+countElements ++;
+let currentElement = document.querySelector(".current");
+
+currentElement.classList.remove("current");
+
+if(countElements > slideshowElements.length){
+slideshowElements[0].classList.add("current");
+}
+else{
+ currentElement.nextElementSibling.classList.add("current");
+}
+
+// currentElement.nextElementSibling.classList.add("current");
+},2000)
+
+
+
+
